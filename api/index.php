f7c45d5519e0b679671f35099fc200757fe83f26
Julius Alvarado <angularjs.uiux@gmail.com>
2017-03-18 17:26:52 -0700

    initial commit

---
+++ api/index.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Julius Alvarado
+ * Date: 3/18/2017
+ * Time: 5:16 PM
+ */
+
+echo 'This is zPHP backend API for the AngularJS client <br><br>';
+echo 'routes:<br><br>';
+echo '~/quiz/get-quiz-results.php<br>';
+echo '~/quiz/get-quiz-results.php?id=xyz<br>';
+
+echo '<hr>';
+
+echo '~/quiz/post-quiz-results.php<br>';
+echo '~/quiz/post-quiz-results.php?id=xyz<br>';
