9951bec3f3e298f7bfea7ef87fbf029f415487c7
Julius Alvarado <angularjs.uiux@gmail.com>
2017-03-19 12:04:52 -0700

    finished styling answer states

--- gulpfile.js
+++ gulpfile.js
@@ -4,14 +4,17 @@
 
 var gulp = require ('gulp');
 var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('default', function(){
     console.log("ello world ^_^/");
+    gulp.watch('styles/**/*.scss', ['styles']);
 });
 
 gulp.task('styles', function(){
     gulp.src('styles/**/*.scss')
-        .pipe(sass())
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer({browsers: ['last 2 versions']}))
         .pipe(gulp.dest('./styles'));
 });
 
